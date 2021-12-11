5067f8883cbb8811d1b9a02ef15814ba8aef5992
Luis Henrique <luish.silveira@hotmail.com>
2021-12-10 21:05:47 -0300

    Personalizando retorno validação

---
+++ src/main/java/br/com/luish/cursomc/resources/exceptions/ValidationError.java
@@ -0,0 +1,24 @@
+package br.com.luish.cursomc.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> list = new ArrayList<>();
+
+	public ValidationError(Integer time, String msg, Long timeStamp) {
+		super(time, msg, timeStamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return list;
+	}
+
+	public void addError(String fieldName, String message) {
+		list.add(new FieldMessage(fieldName, message));
+	}
+
+}
